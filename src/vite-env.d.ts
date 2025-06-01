83eeb9e14bd95e11cbfe7803fd84a4506dce0c18
Atos Alves <atosalvesqueiroz@gmail.com>
2025-05-31 21:23:12 -0300

    feat: adiciona interfaces para variáveis de ambiente no Vite

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_AVATAR_URL: string;
+    readonly VITE_REPOS_URL: string;
+    readonly VITE_GITHUB_URL: string;
+    readonly VITE_LINKEDIN_URL: string;
+    readonly VITE_GMAIL: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
