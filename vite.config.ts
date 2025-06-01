4804ec107c0ba7dededdb7f9c0cfdef87b700d26
Atos Alves <atosalvesqueiroz@gmail.com>
2025-05-31 21:44:12 -0300

    remove: remove base do vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,4 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [TanStackRouterVite({ target: "react", autoCodeSplitting: true }), react(), tailwindcss()],
-    base: "/atosalves-portifolio",
 });
